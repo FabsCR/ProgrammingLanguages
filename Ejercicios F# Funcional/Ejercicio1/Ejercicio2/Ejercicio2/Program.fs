003d76593758ccfe7e4b2e7c3af31a8f344c0954
Fabián Fernández <99295085+FabsCR@users.noreply.github.com>
2023-09-16 06:17:02 -0600

    Add files via upload

---
+++ Ejercicios F# Funcional/Ejercicio1/Ejercicio2/Ejercicio2/Program.fs
@@ -0,0 +1,11 @@
+﻿// Made by Fabián José Fernández Fernández
+
+let filterBySubstring (substring: string) (strings: string list) : string list =
+    strings
+    |> List.filter (fun str -> str.Contains(substring))
+
+// Example usage
+let exampleStrings = ["la casa"; "el perro"; "pintando la cerca"]
+let filteredStrings = filterBySubstring "la" exampleStrings
+
+printfn "Filtered strings: %A" filteredStrings
