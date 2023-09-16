4a674106409dc40905a04b8d4eab3a4fbf226afa
Fabián Fernández <99295085+FabsCR@users.noreply.github.com>
2023-09-16 06:25:51 -0600

    exercise 3 done

---
+++ Ejercicios F# Funcional/Ejercicio1/Ejercicio3/Ejercicio3/Program.fs
@@ -0,0 +1,16 @@
+﻿// Made by Fabián José Fernández Fernández
+
+let nth (index: int) (list: int list) : int option =
+    let indexes = [0 .. List.length list - 1]
+    let indexedList = List.zip indexes list
+    List.tryFind (fun (i, _) -> i = index) indexedList
+    |> Option.map snd
+
+let exampleList = [1; 2; 3; 4; 5]
+let result1 = nth 2 exampleList
+let result2 = nth 3 exampleList
+let result3 = nth 6 exampleList
+
+printfn "nth 2: %A" result1
+printfn "nth 3: %A" result2
+printfn "nth 6: %A" result3
